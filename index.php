a48a244351899be236948bb7ed8d6152f38e2943
Phill Conrad <pconrad@cs.ucsb.edu>
2018-01-29 16:36:09 -0800

    old index.php

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Phill Conrad, UCSB CS Dept.</title>
+
+
+<link href="pconradHomePage.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+   .courseTable tr th {text-align:right; font-weight:normal;}
+#ofcHrsWidget { height: auto; width: 90%; padding:0px; margin:1em;}
+#ofcHrsWidget iframe {margin:0px; padding:0px;}
+</style>
+
+<!--[if IE]>
+<style type="text/css"> 
+/* place css fixes for all versions of IE in this conditional comment */
+.twoColHybLtHdr #sidebar1 { padding-top: 30px; }
+.twoColHybLtHdr #mainContent { zoom: 1; padding-top: 15px; }
+/* the above proprietary zoom property gives IE the hasLayout it may need to avoid several bugs */
+</style>
+<![endif]-->
+<style type="text/css">
+<!--
+.style1 {font-size: 80%}
+-->
+</style>
+</head>
+
+<body class="twoColHybLtHdr">
+
+<div id="container">
+  <div id="header"><div id="hdrImg"><img src="images/PhillHeadShot.jpg" alt="Picture of Phill Conrad" longdesc="http://www.cs.ucsb.edu/~pconrad" /></div>
+    <h1>Phill Conrad</h1>
+    <h2 class="smallSubHead">Lecturer (SOE)<br /> 
+     <a href="http://www.ucsb.edu">University of California, Santa Barbara</a>     </h2>
+    <h2 class="smallSubHead"><a href="http://www.cs.ucsb.edu">Dept. of Computer Science</a> (College of Engineering) and<br /> 
+      <a href="http://www.ccs.ucsb.edu">College of Creative Studies</a> (Joint Appointment)</h2>
+  <!-- end #header --></div>
+  <div id="sidebar1">
+    <p><strong><a href="ccs">CCS Courses</a><br />
+      <br />
+    </strong>various <a href="topics">topics</a></p>
+    <p><strong>CS 
+      Courses<br />
+    </strong></p>
+   <table class="courseTable">
+   <tr> <th><a href="./cs56">CS56:</a></th>     
+   <td>14W,<br/>12S,12W,<br/> 11S,11W</td>    
+   </tr>
+   <tr> <th><a href="./cs8">CS8:</a></th>     
+   <td>14S,13F,<br/>
+       10F,10M,09M</td>    
+   </tr>
+   <tr> <th><a href="./cs16">CS16:</a></th>   <td>14F,<br />
+                                                  10S,10W,09F</td></tr>
+   <tr> <th><a href="./cs10">CS10:</a></th>   <td>09S</td>        </tr>
+   <tr> <th><a href="./cs5nm">CS5nm:</a></th> <td>08F</td>        </tr>
+   <tr> <th><a href="./cs40">CS40:</a></th>   
+   <td>13W,12F,<br />
+     11F,08S</td>        
+   </tr>
+  </table>
+  </div>  <!-- end #sidebar1 -->
+  <div id="mainContent" >
+<!--  <div id="ofcHrsWidget" title="widget to read next n events from a Google Calendar written by Matthew K. Schartman"><p style="text-align:center; margin:0px; padding:0px;">Next Five Office Hours Appointments</p>
+<iframe src="http://pconrad.us/ofchrs" width="99%" style="border:none; margin-left:auto;margin-right:auto;">...</iframe></div> -->
+
+    <p>Office Hours for my current courses, <br />
+    and for CS Undergraduate Advising</p>
+    <ul>
+      <li>See <a href="http://www.cs.ucsb.edu/~pconrad/ofchrs/">http://www.cs.ucsb.edu/~pconrad/ofchrs/</a></li>
+      <li>Also available by appointment: email pconrad@cs.ucsb.edu</li>
+    </ul>
+    <h2 style="clear:right;">What I do</h2>
+    <p>My primary responsibilities include</p>
+    <ul>
+      <li>teaching undergraduate  Computer Science courses in both:
+        <ul>
+          <li>the College of Engineering</li>
+          <li>the College of Creative Studies</li>
+        </ul>
+      </li>
+      <li>serving as a liaison between the Computer Science programs in both colleges.</li>
+      <li>serving as the Undergraduate Adviser for the Computer Science Department.</li>
+    </ul>
+    <p>I  am interested in exploring more effective ways to introduce students to computing,  to teach the introductory courses in Computer Science.</p>
+    <p>My other areas of expertise include:</p>
+    <ul>
+      <li> Computer Networks and Communication, particularly
+        <ul>
+          <li>transport protocols</li>
+          <li> multimedia communication</li>
+        </ul>
+      </li>
+      <li>Web Application development</li>
+    </ul>
+    <h2>More:</h2>
+    <ul>
+      <li>Some (slightly out of date) materials such as my CV, Research and Teaching Statement can be found here: <a href="review2011/">review2011</a></li>
+      <li><a href="http://copland.udel.edu/~pconrad">my previous web site</a> at the University of Delaware.</li>
+      <li><a href="erdos">My Erdös number is at most 5</a></li>
+    </ul>
+  </div>
+  <div id="footer">
+    <img id="footerImg" src="images/UCSB/sbbeach.195w.100h.jpg" alt="UCSB Beach" longdesc="http://www.ucsb.edu" />
+    <p>Phill Conrad, Lecturer (SOE)<br />
+      <a href="http://www.ucsb.edu">University of California, Santa Barbara</a><br />
+      <br />
+      pconrad at cs.ucsb.edu<br />
+      Office: Harold Frank Hall 1113
+      <!-- end #footer -->
+    </p>
+  </div>
+<!-- end #container --></div>
+</body>
+</html>
